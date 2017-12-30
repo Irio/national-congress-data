4fd10176826c235f9a087d8f98c6fbfa8675aa0d
Irio Musskopf <iirineu@gmail.com>
2017-12-30 11:57:39 -0200

    Ongoing work

--- data_collection/data_collection/spiders/chamber_of_deputies_sessions.py
+++ data_collection/data_collection/spiders/chamber_of_deputies_sessions.py
@@ -18,19 +18,30 @@ class ChamberOfDeputiesSessionsSpider(scrapy.Spider):
 
     def parse(self, response):
         iframe_url = response.css('#ifr::attr("src")').extract_first()
-        yield scrapy.Request(iframe_url, callback = self.parse_iframe)
+        yield scrapy.Request(iframe_url, callback=self.submit_search_query)
 
-    def parse_iframe(self, response):
+    def submit_search_query(self, response):
+        return FormRequest.from_response(
+            response,
+            formdata={'datInicial': '01/01/2017', 'datFinal': '31/12/2017'},
+            formname='form1',
+            callback=self.parse_session_list,
+        )
+
+    def parse_session_list(self, response):
         selector = (
-            '//div[contains(concat(" ", @class, " "), " caixaDownload ")]'
-            '//a[contains(text(),"legislatura")]'
+            '//ul[contains(concat(" ", @class, " "), " listaMarcada ")]'
+            '//a[contains(text(), "votantes por UF")]'
         )
         urls = response.xpath(selector).xpath('@href').extract()
         for url in urls:
             url = response.urljoin(url)
-            yield scrapy.Request(url, callback=self.parse_download_page)
+            yield scrapy.Request(url, callback=self.parse_session)
 
-    def parse_download_page(self, response):
+    def parse_session(self, response):
+        from scrapy.utils.response import open_in_browser
+        open_in_browser(response)
+        import ipdb; ipdb.set_trace()
         term = response.css('b::text').extract_first()
         term = re.findall('\d{4}', term)[0]
         files = response.css('[name="liste1"] option::attr("value")').extract()
